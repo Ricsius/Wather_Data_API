6807ae90e077fbb9b65f7dc9a5248b06dfdb199d
Ricsius <hajnal.ricsi@gmail.com>
2024-11-19 16:52:49 +0100

    Add URL endpoint for yearly and all data

--- main.py
+++ main.py
@@ -17,20 +17,40 @@ stations[NEW_STATION_ID_COLUMN] = stations[STATION_ID_COLUMN]
 stations[NEW_STATION_NAME_COLUMN] = stations[STATION_NAME_COLUMN]
 stations = stations[[NEW_STATION_ID_COLUMN, NEW_STATION_NAME_COLUMN]]
 
+def read_station(id):
+    path = f"{DATA_PATH}\TG_STAID" + str(id).zfill(6) + ".txt"
+    df = pd.read_csv(path, skiprows=20, parse_dates=[DATE_COLUMN])
+
+    return df
+
 @app.route("/")
 def home():
     return render_template("home.html", data=stations.to_html())
 
 @app.route("/api/v1/<station>/<date>")
 def get(station, date):
-    path = f"{DATA_PATH}\TG_STAID" + str(station).zfill(6) + ".txt"
-    df = pd.read_csv(path, skiprows=20, parse_dates=[DATE_COLUMN])
-
+    df = read_station(station)
     temperature = df.loc[df[DATE_COLUMN] == date][TEMPERATURE_COLUMN].squeeze() / 10
 
     return {"station": station,
             "date": date,
             "temperature": temperature}
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    df = read_station(station)
+    result = df.to_dict(orient="records")
+
+    return result
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+    year = str(year)
+    df = read_station(station)
+    df[DATE_COLUMN] = df[DATE_COLUMN].astype(str)
+    result = df[df[DATE_COLUMN].str.startswith(year)].to_dict(orient="records")
+
+    return result
 
 if __name__ == "__main__":
     app.run(debug=True)
